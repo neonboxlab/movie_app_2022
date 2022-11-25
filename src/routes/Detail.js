da1605f53505c7cf17594403ae027746b08a7026
neonboxlab <inthemode32@gmail.com>
2022-11-25 19:58:09 +0900

    #1.0 Creating your first React App

---
+++ src/routes/Detail.js
@@ -0,0 +1,38 @@
+import { useEffect, useState, useCallback } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
+
+function Detail() {
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovies] = useState([]);
+	const {id} = useParams();
+    const getMovie = useCallback(async () => {
+        const json = await(
+            await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+        ).json();
+        setMovies(json.data.movie)
+        setLoading(false);
+    }, [id]);
+    useEffect(() => {
+        getMovie();
+    }, [getMovie]);
+	return (
+        <div>
+            {loading ? (
+                <h1>Loading...</h1> 
+            ) : (
+                <div>
+                    <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    coverImg={movie.medium_cover_image}
+                    title={movie.title}
+                    summary={movie.summary}
+                    genres={movie.genres}
+                    />
+                </div>
+            )}
+        </div>
+    );
+}
+export default Detail;
